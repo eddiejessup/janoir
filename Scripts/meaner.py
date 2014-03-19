b3d994b859728a43ace2426233207e01055815b8
Elliot Marsden <elliot.marsden@gmail.com>
2014-03-19 23:48:26 +0000

    Add script to mean multiple leaving time seeds

---
+++ Scripts/meaner.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as pp
+import argparse
+import sys
+import scipy.stats
+
+ts, als = [], []
+for fname in sys.argv[1:-1]:
+    t, al = np.loadtxt(fname, unpack=True)
+    ts.append(t)
+    als.append(al)
+
+als = np.array(als)
+ts = np.array(ts)
+
+np.savetxt(sys.argv[-1], zip(np.mean(ts, axis=0), np.nanmean(als, axis=0), scipy.stats.sem(als, axis=0)))
