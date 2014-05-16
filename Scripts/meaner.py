3df98555fd286a5f941d2818e55790be2ea2ef5d
Elliot Marsden <elliot.marsden@gmail.com>
2014-05-16 14:23:39 +0100

    Clean up data mean script, and add bit to calculate median

--- Scripts/meaner.py
+++ Scripts/meaner.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python
+
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as pp
 import argparse
 import sys
 import scipy.stats
 
-ts, als = [], []
-for fname in sys.argv[1:-1]:
-    t, al = np.loadtxt(fname, unpack=True)
-    ts.append(t)
-    als.append(al)
 
-als = np.array(als)
-ts = np.array(ts)
+def mean(fnames, out):
+    ts, als = [], []
+    for fname in fnames:
+        t, al = np.loadtxt(fname, unpack=True)
+        ts.append(t)
+        als.append(al)
 
-np.savetxt(sys.argv[-1], zip(np.mean(ts, axis=0), np.nanmean(als, axis=0), scipy.stats.sem(als, axis=0)))
+    als = np.array(als)
+    ts = np.array(ts)
+
+    np.savetxt(
+        # out, zip(np.mean(ts, axis=0), np.nanmean(als, axis=0), scipy.stats.sem(als, axis=0)))
+        out, zip(np.mean(ts, axis=0), np.median(als, axis=0), scipy.stats.sem(als, axis=0)))
+
+parser = argparse.ArgumentParser(description='Mean fnames')
+parser.add_argument('fnames', nargs='*')
+parser.add_argument('-o', '--out')
+args = parser.parse_args()
+
+mean(args.fnames, args.out)
